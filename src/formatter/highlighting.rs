11e0ae6df1b9a272dd5f6b7f9cb2d92cf3baf5f9
Sakurai <K.Sakurai00@outlook.com>
2025-08-08 23:17:14 +0900

    feat(highlighting): add unit tests

--- src/formatter/highlighting.rs
+++ src/formatter/highlighting.rs
@@ -86,3 +86,64 @@ fn apply_style(text: &str, text_color: &Color, text_style: &Style) -> ColoredStr
 
     styled_text
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use colored::Colorize;
+
+    #[test]
+    fn test_get_highlight_rules() {
+        let rules = get_highlight_rules();
+        assert!(rules.is_ok());
+        assert_eq!(rules.unwrap().len(), 3);
+    }
+
+    #[test]
+    fn test_apply_style() {
+        let text = "test";
+        let colored_text = apply_style(text, &Color::Red, &Style::Bold);
+        assert_eq!(colored_text, "test".red().bold());
+    }
+
+    #[test]
+    fn test_apply_highlighting_no_match() {
+        let rules = get_highlight_rules().unwrap();
+        let line = "this is a normal line";
+        let highlighted_line = apply_highlighting(line, &rules);
+        assert_eq!(highlighted_line, line);
+    }
+
+    #[test]
+    fn test_apply_highlighting_single_match() {
+        let rules = get_highlight_rules().unwrap();
+        let line = "this is a CRITICAL line";
+        let highlighted_line = apply_highlighting(line, &rules);
+        assert_eq!(
+            highlighted_line,
+            "this is a ".to_string() + &"CRITICAL".bright_red().bold().to_string() + " line"
+        );
+    }
+
+    #[test]
+    fn test_apply_highlighting_multiple_matches() {
+        let rules = get_highlight_rules().unwrap();
+        let line = "this is a CRITICAL line with a WARN";
+        let highlighted_line = apply_highlighting(line, &rules);
+        assert_eq!(
+            highlighted_line,
+            "this is a ".to_string()
+                + &"CRITICAL".bright_red().bold().to_string()
+                + " line with a "
+                + &"WARN".yellow().underline().to_string()
+        );
+    }
+
+    #[test]
+    fn test_apply_highlighting_empty_string() {
+        let rules = get_highlight_rules().unwrap();
+        let line = "";
+        let highlighted_line = apply_highlighting(line, &rules);
+        assert_eq!(highlighted_line, "");
+    }
+}
